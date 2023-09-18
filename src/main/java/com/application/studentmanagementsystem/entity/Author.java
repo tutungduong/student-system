51782777b878b6d37b73be8173a0f85b1c3215d5
c0mr4de <c0mr4dex@hotmail.com>
2023-09-19 00:44:17 +0700

    update

---
+++ src/main/java/com/application/studentmanagementsystem/entity/Author.java
@@ -0,0 +1,23 @@
+package com.application.studentmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false)
+    private String description;
+}
